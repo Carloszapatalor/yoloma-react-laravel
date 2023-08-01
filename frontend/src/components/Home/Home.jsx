f868a9e6d68c260ef0a310833a773068b7486458
Aleromero77 <aleowens77@gmail.com>
2023-08-01 17:07:54 -0600

    Inicio 01/08/2023

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../../App.css';
+import '../../App.css'; // Importamos App.css para usar estilos desde allí
 
 const Home = () => {
-      return (
-    <div className="container">
-      <header className="mt-4">
-        <h1>Bienvenido al expediente clínico Yoloma</h1>
-      </header>
-      <main className="mt-4">
-        <button className="btn btn-primary">Haz clic aquí</button>
-      </main>
+  const buttonStyle = {
+    marginRight: '10px',
+  };
+
+  return (
+    <div className="home-container background-image"> {/* Aplicamos la clase background-image directamente al componente Home */}
+      <h1>Bienvenido</h1>
+      <div className="mt-4">
+        <button className="btn btn-secondary">Iniciar sesión</button>
+      </div>
     </div>
-      )
+  );
 };
 
 export default Home;
